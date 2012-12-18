e7d136cf091ae68392761b4b1436b9d5a140a01d
Ulrike Muenzner <ulrike@floria>
2012-12-18 16:03:07 +0100

    kind of works for biographer generated svg files.

--- makemysvgmorestylishplease.py
+++ makemysvgmorestylishplease.py
@@ -2,14 +2,38 @@
 
 #import sys
 
-svg = open('ugly.svg').read()
+svg = open('ugly_er.svg').read()
 
-if '<style type="text/css"></style>' in svg:
-	print "Warning: empty stylesheet, probably you have a problem with colors in your picture'
+#if '<style type="text/css"></style>' in svg:
+#	print "Warning: empty stylesheet, probably you have a problem with colors in your picture'
 
-#svg = svg.replace('><', '>\n<')
+svg = svg.replace('><', '>\n<')
 
 #svg = svg.replace('<g>\n<g ', '<g ').replace('</g>\n</g>\n</svg>', '</g>\n</svg>')
+
+a = '''
+.statevariable {
+	font-size: 10px;
+}
+'''
+b = '''
+.VariableValue{
+    fill: none;
+    stroke: black;
+    }
+
+.statevariable {
+font-size: 10px;
+    fill: none;
+    stroke: black
+}
+.rect {
+    fill: none;
+    stroke: black;
+}
+'''
+
+svg = svg.replace(a, b)
 
 open('pretty.svg', 'w').write(svg)
 
